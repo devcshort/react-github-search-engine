965c4c78fa52c02c25983b29de7ee19ac9f36a47
Vignesh Raj <53948153+vigneshrajj@users.noreply.github.com>
2021-03-06 14:23:42 +0530

    ui improvised

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 // import custom components
-import Profile from './Components/Profile/Profile';
-import Footer from './Components/Footer/Footer';
+import Profile from "./Components/Profile/Profile";
+import Footer from "./Components/Footer/Footer";
 // import material-ui components
-import { TextField, Button, CircularProgress as Loader } from '@material-ui/core';
+import {
+  TextField,
+  Button,
+  CircularProgress as Loader
+} from "@material-ui/core";
 
 class App extends Component {
   state = {
-    username: '',
-    image: '',
+    username: "",
+    image: "",
     searched: false,
     repos: [],
     loading: false
-  }
+  };
 
   onFormSubmit = (e) => {
     e.preventDefault();
@@ -24,38 +28,39 @@ class App extends Component {
 
     var avatar_url;
 
-    axios.get(`https://api.github.com/users/${this.state.username}`)
-    .then(res => {
-      avatar_url = res.data.avatar_url;
-      return axios.get(res.data.repos_url);
-    })
-    .then(res => {
-      this.setState({
-        loading: false,
-        repos: res.data,
-        image: avatar_url,
-        searched: true
+    axios
+      .get(`https://api.github.com/users/${this.state.username}`)
+      .then((res) => {
+        avatar_url = res.data.avatar_url;
+        return axios.get(res.data.repos_url);
+      })
+      .then((res) => {
+        this.setState({
+          loading: false,
+          repos: res.data,
+          image: avatar_url,
+          searched: true
+        });
+      })
+      .catch(() => {
+        this.setState({
+          loading: false
+        });
       });
-    })
-    .catch(() => {
-      this.setState({
-        loading: false
-      });
-    });
-  }
+  };
 
-  onUsernameChange = e => {
+  onUsernameChange = (e) => {
     this.setState({
       searched: false,
       username: e.target.value
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <div className="formContainer">
-          <h1 style={{ marginTop: 0 }}>{ process.env.REACT_APP_NAME }</h1>
+          <h1>Github Search Engine</h1>
           <form
             style={{ alignItems: "flex-end", display: "flex" }}
             onSubmit={this.onFormSubmit}
@@ -69,26 +74,21 @@ class App extends Component {
             <Button type="submit">Search</Button>
           </form>
 
-          {
-            this.state.loading
-              ? <Loader style={{ display: "block", margin: "15px auto" }} />
-              : null
-          }
+          {this.state.loading ? (
+            <Loader style={{ display: "block", margin: "15px auto" }} />
+          ) : null}
 
-          {
-            this.state.searched
-              && (
-                <Profile
-                  username={this.state.username}
-                  image={this.state.image}
-                  repos={this.state.repos}
-                />
-              )
-          }
+          {this.state.searched && (
+            <Profile
+              username={this.state.username}
+              image={this.state.image}
+              repos={this.state.repos}
+            />
+          )}
         </div>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
